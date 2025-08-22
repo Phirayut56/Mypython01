c22991647d4a5b517fdef5a07104d909424abaf0
Phirayut56 <phirayut5678@gmail.com>
2025-08-22 12:13:32 +0700

    V.3

---
+++ test06.py
@@ -0,0 +1,32 @@
+# operator ตัวดำเนินการ คือ เครื่องหมายที่ใช้ในการประมวณผลข้อมูล
+# 1. Arithmetic Operator เครื่องหมายคำนวณ
+# ( +, -, *, /, % mod, // floor , ** eponential)
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 % 3)  # หารเอาเศษ
+print(10 // 3) # หารเอาส่วน
+print(10 ** 3) 
+print("++++++++++++++++++++++++")
+# 2. Comparison Operator เครื่องหมายเปรียบเทียบ (==, !=, >, <, >=, <=)
+# ผลลัพธ์ที่จะได้แค่ True/False
+print(10 == 3)
+print(10 != 3)
+print('Bangna' >= 'Bangpoo')
+print('Thailand' < 'THAILAND')
+print("++++++++++++++++++++++++")
+# 3.logical Operator เครื่งหมายตรรกะ not, and, or
+# ข้อมูลที่จะใช้กับเครื่องหมายนี้จะต้องเป็น True/False และผลลัพธ์ที่ได้จากการใฃ้เครื่องหมาย True/False
+print(not True)
+print(not False)
+print('-----------')
+print(True and True) #Truse
+print(True and False)
+print(False and True)
+print(False and False)
+print('------------')
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False) #False
