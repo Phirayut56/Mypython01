68df7d4271fb0681879db875c24a065ea44763b6
Phirayut56 <phirayut5678@gmail.com>
2025-08-29 11:46:33 +0700

    Do control flow selection complete

---
+++ test14.py
@@ -0,0 +1,17 @@
+# if-elif-else
+
+score = 79.99
+
+if score >= 80 :
+    print('Grade A')
+elif score >= 70 :
+    print('Grade B')
+elif score >= 60 :
+    print('Grade C')
+elif score >= 50 :
+    print('Grade D')
+else :
+    print('Grade F')
+
+print('Bey bey....')
+print('Ei ei......')
